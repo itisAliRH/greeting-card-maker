384c290b637e67a926eeb9afb14f4aa05cc2dab5
Alireza Heidari <itisalirh@gmail.com>
2024-12-27 01:09:42 +0100

    Adds compatibility date to Nuxt config
    Includes a compatibility date to ensure future compatibility
    Enables better control over framework updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+
   devtools: {
     enabled: true,
   },
+
   modules: ["@nuxtjs/tailwindcss"],
+  compatibilityDate: "2024-12-27",
 });
